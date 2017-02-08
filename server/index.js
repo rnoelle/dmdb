f0471fc96aa383d668d71bb7f6b05f3be7eeae74
rnoelle <reidnoelle2@gmail.com>
2017-02-08 15:57:32 -0700

    changed ng-if logic on index.html

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+const express = require('express');
+const bodyParser = require('body-parser');
 
-import movies from './movies';
-import celebs from './celebs';
+const movies = require('./movies');
+const celebs = require('./celebs');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.get('/movies', function (req, res, next) {
-  
+  res.status(200).send(movies);
 })
 app.get('/celebs', function (req, res, next) {
-
+  res.status(200).send(celebs);
 })
 
 app.listen(3000, function () {
